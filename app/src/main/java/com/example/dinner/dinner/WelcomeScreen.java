d503066b0b3d40ac6b9a3249628f949c7f29dec3
Ahris <alicewang54@gmail.com>
2014-09-13 12:26:42 -0500

    Added Android Files

---
+++ app/src/main/java/com/example/dinner/dinner/WelcomeScreen.java
@@ -0,0 +1,49 @@
+package com.example.dinner.dinner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class WelcomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_welcome_screen);
+
+        TextView txt = (TextView) findViewById(R.id.title);
+        Typeface font = Typeface.createFromAsset(getAssets(), "Pacifico.ttf");
+        txt.setTypeface(font);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.welcome_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void openOptionsPage(View view) {
+        // Open up the options page
+        Intent intent = new Intent(this, FoodOptions.class);
+    }
+}
